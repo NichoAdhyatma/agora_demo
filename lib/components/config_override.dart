c3d7f6c10e87d721e4c4783ff400fa4a5becdc70
NichoAdhyatma <nicholausadhyatma@gmail.com>
2024-11-19 15:33:03 +0700

    build for web

--- lib/components/config_override.dart
+++ lib/components/config_override.dart
@@ -1,10 +1,4 @@
-// Fill in the app ID obtained from the Agora Console
-const appId = "9904937daa4b4dea818b3ff574e5d5be";
-// Fill in the temporary token generated from Agora Console
-const token = "007eJxTYNDKWM7rWKqz6755wy8LI6/zJc+zZswXyOsOl2nkmTe5zF6BwdLSwMTS2DwlMdEkySQlNdHC0CLJOC3N1Nwk1TTFNClVTN8mvSGQkcG4PYKFkQECQXxWhqTUpMRiBgYAvk4c+A==";
-// Fill in the channel name you used to generate the token
-const channel = "bebas";
-
+import 'package:agora_sdk_example/secret.dart';
 
 /// Key of APP ID
 const keyAppId = 'TEST_APP_ID';
---
+++ lib/secret.dart
@@ -0,0 +1,6 @@
+// Fill in the app ID obtained from the Agora Console
+const appId = "9904937daa4b4dea818b3ff574e5d5be";
+// Fill in the temporary token generated from Agora Console
+const token = "007eJxTYNDKWM7rWKqz6755wy8LI6/zJc+zZswXyOsOl2nkmTe5zF6BwdLSwMTS2DwlMdEkySQlNdHC0CLJOC3N1Nwk1TTFNClVTN8mvSGQkcG4PYKFkQECQXxWhqTUpMRiBgYAvk4c+A==";
+// Fill in the channel name you used to generate the token
+const channel = "bebas";
